84b30e45132cb0a08266476294f0e3ea32c11b56
Viacheslav Zalitskyi <slaw.z2014@gmail.com>
2022-10-20 16:25:47 -0700

    Device create

--- simple-windows-driver/driver.c
+++ simple-windows-driver/driver.c
@@ -1,14 +1,30 @@
 #include "driver.h"
 
+//32768 - 65535
+#define _DEVICE_TYPE 32768 
+
+PUNICODE_STRING name;
 
 NTSTATUS DriverEntry(
 	_In_ PDRIVER_OBJECT driverObject,
 	_In_ PUNICODE_STRING registryPath
-) 
+)
 {
-	DbgPrintEx(0,0,"[simple-windows-driver] Driver load");
+	DbgPrintEx(0, 0, "[simple-windows-driver] Driver load");
 	UNREFERENCED_PARAMETER(registryPath);
-	driverObject->DriverUnload = DriverUnload;
+	RtlInitUnicodeString(name, L"\Device\Simple2"); //copy  L"\Device\Simple2" to name as unicode
+	driverObject->DriverUnload = DriverUnload; //pointer to unload function
+	PDEVICE_OBJECT deviceObject;
+	NTSTATUS status = IoCreateDevice(
+		driverObject, 0,
+		name,
+		_DEVICE_TYPE,
+		0, FALSE,
+		0
+	);
+	if (!NT_SUCCESS(status))
+		return status;
+	
 	return STATUS_SUCCESS;
 }
 
@@ -19,3 +35,4 @@ void DriverUnload(
 
 	UNREFERENCED_PARAMETER(driverObject);
 }
+

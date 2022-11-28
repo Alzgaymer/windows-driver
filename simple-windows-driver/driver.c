9509f875a8b89441f406ee1cca110955d2f7669f
Viacheslav Zalitskyi <viacheslav.zalitkiy@gmail.com>
2022-11-28 15:30:07 -0800

    neither add

--- simple-windows-driver/driver.c
+++ simple-windows-driver/driver.c
@@ -9,7 +9,7 @@ PDEVICE_OBJECT DeviceObject = NULL;
 #define DEVICE_SEND_NEITHER CTL_CODE(FILE_DEVICE_UNKNOWN, 0x803, METHOD_NEITHER, FILE_WRITE_DATA)
 
 #define DEVICE_REC_BUFF CTL_CODE(FILE_DEVICE_UNKNOWN, 0x804, METHOD_BUFFERED, FILE_READ_DATA)
-#define DEVICE_REC_DIRECT CTL_CODE(FILE_DEVICE_UNKNOWN, 0x805, METHOD_IN_DIRECT, FILE_READ_DATA)
+#define DEVICE_REC_DIRECT CTL_CODE(FILE_DEVICE_UNKNOWN, 0x805, METHOD_OUT_DIRECT, FILE_READ_DATA)
 #define DEVICE_REC_NEITHER CTL_CODE(FILE_DEVICE_UNKNOWN, 0x806, METHOD_NEITHER, FILE_READ_DATA)
 
 NTSTATUS DriverEntry(
@@ -102,25 +102,74 @@ NTSTATUS WinDrvDispatchCTL(PDEVICE_OBJECT DeviceObject, PIRP irp)
 	NTSTATUS status = STATUS_SUCCESS;
 	
 	ULONG returnLength = 0;
-	PVOID buffer = irp->AssociatedIrp.SystemBuffer;
-	ULONG inLength = pirps->Parameters.DeviceIoControl.InputBufferLength;
-	ULONG outLength = pirps->Parameters.DeviceIoControl.OutputBufferLength;
+
+	PVOID buffer = irp->AssociatedIrp.SystemBuffer;						//METHOD_BUFFERED		 buffer
+	PVOID dirbuffer = irp->MdlAddress;									//METHOD_IN(OUT)_DIRECT	 buffer
+	PVOID nbuffout = pirps->Parameters.DeviceIoControl.Type3InputBuffer;//METHOD_NEITHER (input) buffer
+	PVOID nbuffin = irp->UserBuffer;									//METHOD_NEITHER (output)buffer
+	PMDL mdl = NULL;
+	ULONG inBufLength = pirps->Parameters.DeviceIoControl.InputBufferLength;
+	ULONG outBufLength = pirps->Parameters.DeviceIoControl.OutputBufferLength;
 	WCHAR* demo_buff = L"text from driver(method buffered)", demo_direct = L"text from driver(method direct)", demo_neither = L"text from driver(method neither)";
+	
+	if (!inBufLength || !outBufLength)
+	{
+		status = STATUS_INVALID_PARAMETER;
+		irp->IoStatus.Status = status;
+
+		IoCompleteRequest(irp, IO_NO_INCREMENT);
+
+		return status;
+	}
+	
 	switch (pirps->Parameters.DeviceIoControl.IoControlCode)
 	{
 	case DEVICE_SEND_BUFF:
 		//KdPrint(("send data is %ws \r\n"),buffer);
-		DbgPrnt(("win-driver: "__FUNCTION__": request: read: method: buffered"));
+		DbgPrnt(("win-driver: "__FUNCTION__": request: read: method: buffered: message: %ws \r\n",buffer));
 		returnLength = LEN(buffer);
 		break;
 	case DEVICE_SEND_DIRECT:
 		//KdPrint(("send data is %ws \r\n"),buffer);
-		DbgPrnt(("win-driver: "__FUNCTION__": request: read: method: direct"));
+		DbgPrnt(("win-driver: "__FUNCTION__": request: read: method: direct: message %ws\r\n", NULL));
 		returnLength = LEN(buffer);
 		break;
 	case DEVICE_SEND_NEITHER:
-		//KdPrint(("send data is %ws \r\n"),buffer);
-		DbgPrnt(("win-driver: "__FUNCTION__": request: read: method: neither"));
+		try {
+			ProbeForRead(nbuffin, inBufLength, sizeof(WCHAR));
+		}
+		except(EXCEPTION_EXECUTE_HANDLER)
+		{
+			status = GetExceptionCode();
+			break;
+		}
+		mdl = IoAllocateMdl(nbuffin, inBufLength, FALSE, TRUE, NULL);
+		if (!mdl)
+		{
+			status = STATUS_INSUFFICIENT_RESOURCES;
+			break;
+		}
+		try
+		{
+			MmProbeAndLockPages(mdl, UserMode, IoReadAccess);
+		}
+		except(EXCEPTION_EXECUTE_HANDLER)
+		{
+			status = GetExceptionCode();
+			IoFreeMdl(mdl);
+			break;
+		}
+		buffer = MmGetSystemAddressForMdlSafe(mdl, NormalPagePriority | MdlMappingNoExecute);
+		if (!buffer) {
+			status = STATUS_INSUFFICIENT_RESOURCES;
+			MmUnlockPages(mdl);
+			IoFreeMdl(mdl);
+			break;
+		}
+		DbgPrnt(("win-driver: "__FUNCTION__": request: read: method: neither: message: %ws",buffer));
+		MmUnlockPages(mdl);
+		IoFreeMdl(mdl);
+		//KdPrint(("send data is %ws \r\n"),buffer);		
 		returnLength = LEN(buffer);
 		break;
 	case DEVICE_REC_BUFF:
@@ -134,7 +183,30 @@ NTSTATUS WinDrvDispatchCTL(PDEVICE_OBJECT DeviceObject, PIRP irp)
 		returnLength = LEN(buffer);
 		break;
 	case DEVICE_REC_NEITHER:
-		wcsncpy(buffer, demo_neither, 511);
+		mdl = IoAllocateMdl(nbuffout, outBufLength, FALSE, TRUE, NULL);
+		if (!mdl)
+		{
+			status = STATUS_INSUFFICIENT_RESOURCES;
+			break;
+		}
+		try {
+			MmProbeAndLockPages(mdl, UserMode, IoWriteAccess);
+		}
+		except(EXCEPTION_EXECUTE_HANDLER)
+		{
+			status = GetExceptionCode();
+			IoFreeMdl(mdl);
+			break;
+		}
+		buffer = MmGetSystemAddressForMdlSafe(mdl, NormalPagePriority | MdlMappingNoExecute);
+		if (!buffer) {
+			MmUnlockPages(mdl);
+			IoFreeMdl(mdl);
+			status = STATUS_INSUFFICIENT_RESOURCES;
+			break;
+		}
+		RtlCopyBytes(buffer, demo_neither, outBufLength);
+		//wcsncpy(nbuffout, demo_neither, outBufLength);
 		DbgPrnt(("win-driver: "__FUNCTION__": request: write: method: neither"));
 		returnLength = LEN(buffer);
 		break;

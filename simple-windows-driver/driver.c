70dbd70f1c813ba5c8f22f65a05a62d904075e70
Viacheslav Zalitskyi <viacheslav.zalitkiy@gmail.com>
2022-11-28 16:10:23 -0800

    clean

--- simple-windows-driver/driver.c
+++ simple-windows-driver/driver.c
@@ -100,122 +100,29 @@ NTSTATUS WinDrvDispatchCTL(PDEVICE_OBJECT DeviceObject, PIRP irp)
 {
 	PIO_STACK_LOCATION pirps = IoGetCurrentIrpStackLocation(irp);
 	NTSTATUS status = STATUS_SUCCESS;
-	
-	ULONG returnLength = 0;
-
-	PVOID buffer = irp->AssociatedIrp.SystemBuffer;						//METHOD_BUFFERED		 buffer
-	PVOID dirbuffer = irp->MdlAddress;									//METHOD_IN(OUT)_DIRECT	 buffer
-	PVOID nbuffout = pirps->Parameters.DeviceIoControl.Type3InputBuffer;//METHOD_NEITHER (input) buffer
-	PVOID nbuffin = irp->UserBuffer;									//METHOD_NEITHER (output)buffer
-	PMDL mdl = NULL;
+	ULONG retLen = 0;
+	PVOID buffer = NULL;
 	ULONG inBufLength = pirps->Parameters.DeviceIoControl.InputBufferLength;
 	ULONG outBufLength = pirps->Parameters.DeviceIoControl.OutputBufferLength;
-	WCHAR* demo_buff = L"text from driver(method buffered)", demo_direct = L"text from driver(method direct)", demo_neither = L"text from driver(method neither)";
-	
-	if (!inBufLength || !outBufLength)
-	{
-		status = STATUS_INVALID_PARAMETER;
-		irp->IoStatus.Status = status;
-
-		IoCompleteRequest(irp, IO_NO_INCREMENT);
-
-		return status;
-	}
-	
+	PMDL mdl = NULL;
+	WCHAR* data = L"text from the win-driver";
 	switch (pirps->Parameters.DeviceIoControl.IoControlCode)
 	{
 	case DEVICE_SEND_BUFF:
-		//KdPrint(("send data is %ws \r\n"),buffer);
-		DbgPrnt(("win-driver: "__FUNCTION__": request: read: method: buffered: message: %ws \r\n",buffer));
-		returnLength = LEN(buffer);
-		break;
-	case DEVICE_SEND_DIRECT:
-		//KdPrint(("send data is %ws \r\n"),buffer);
-		DbgPrnt(("win-driver: "__FUNCTION__": request: read: method: direct: message %ws\r\n", NULL));
-		returnLength = LEN(buffer);
-		break;
-	case DEVICE_SEND_NEITHER:
-		try {
-			ProbeForRead(nbuffin, inBufLength, sizeof(WCHAR));
-		}
-		except(EXCEPTION_EXECUTE_HANDLER)
-		{
-			status = GetExceptionCode();
-			break;
-		}
-		mdl = IoAllocateMdl(nbuffin, inBufLength, FALSE, TRUE, NULL);
-		if (!mdl)
-		{
-			status = STATUS_INSUFFICIENT_RESOURCES;
-			break;
-		}
-		try
-		{
-			MmProbeAndLockPages(mdl, UserMode, IoReadAccess);
-		}
-		except(EXCEPTION_EXECUTE_HANDLER)
-		{
-			status = GetExceptionCode();
-			IoFreeMdl(mdl);
-			break;
-		}
-		buffer = MmGetSystemAddressForMdlSafe(mdl, NormalPagePriority | MdlMappingNoExecute);
-		if (!buffer) {
-			status = STATUS_INSUFFICIENT_RESOURCES;
-			MmUnlockPages(mdl);
-			IoFreeMdl(mdl);
-			break;
-		}
-		DbgPrnt(("win-driver: "__FUNCTION__": request: read: method: neither: message: %ws",buffer));
-		MmUnlockPages(mdl);
-		IoFreeMdl(mdl);
-		//KdPrint(("send data is %ws \r\n"),buffer);		
-		returnLength = LEN(buffer);
+		KdPrint(("send data is %ws \r\n"), buffer);
+		retLen = LEN(buffer);
 		break;
 	case DEVICE_REC_BUFF:
-		wcsncpy(buffer, demo_buff, 511);
-		DbgPrnt(("win-driver: "__FUNCTION__": request: write: method: buffered"));
-		returnLength = LEN(buffer);
-		break;
-	case DEVICE_REC_DIRECT:
-		wcsncpy(buffer, demo_direct, 511);
-		DbgPrnt(("win-driver: "__FUNCTION__": request: write: method: direct"));
-		returnLength = LEN(buffer);
-		break;
-	case DEVICE_REC_NEITHER:
-		mdl = IoAllocateMdl(nbuffout, outBufLength, FALSE, TRUE, NULL);
-		if (!mdl)
-		{
-			status = STATUS_INSUFFICIENT_RESOURCES;
-			break;
-		}
-		try {
-			MmProbeAndLockPages(mdl, UserMode, IoWriteAccess);
-		}
-		except(EXCEPTION_EXECUTE_HANDLER)
-		{
-			status = GetExceptionCode();
-			IoFreeMdl(mdl);
-			break;
-		}
-		buffer = MmGetSystemAddressForMdlSafe(mdl, NormalPagePriority | MdlMappingNoExecute);
-		if (!buffer) {
-			MmUnlockPages(mdl);
-			IoFreeMdl(mdl);
-			status = STATUS_INSUFFICIENT_RESOURCES;
-			break;
-		}
-		RtlCopyBytes(buffer, demo_neither, outBufLength);
-		//wcsncpy(nbuffout, demo_neither, outBufLength);
-		DbgPrnt(("win-driver: "__FUNCTION__": request: write: method: neither"));
-		returnLength = LEN(buffer);
+		wcsncpy(buffer, data, 511);
+		//DbgPrnt(("win-driver: "__FUNCTION__": request: write: method: buffered: data: \r\n"));
+		retLen = LEN(buffer);
 		break;
 	default:
-		status = STATUS_INVALID_PARAMETER;
 		break;
 	}
+
 	irp->IoStatus.Status = status;
-	irp->IoStatus.Information = returnLength;
+	irp->IoStatus.Information = retLen;
 	IoCompleteRequest(irp, IO_NO_INCREMENT);
 	return status;
 }
